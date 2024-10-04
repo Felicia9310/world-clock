130901ae1065eefa43d4601b7a3f2ed173917dea
Felicia9310 <feliciasiboza@gmail.com>
2024-10-04 02:13:26 +0200

    Added js for time and date update

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngelesElement.querySelector(".date");
+  let losAngelesTime = losAngelesElement.querySelector(".time");
+  let laCurrentTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDate.innerHTML = laCurrentTime.format("D MMMM YYYY");
+  losAngelesTime.innerHTML = laCurrentTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDate = sydneyElement.querySelector(".date");
+  let sydneyTime = sydneyElement.querySelector(".time");
+  let syCurrentTime = moment().tz("Australia/Sydney");
+
+  sydneyDate.innerHTML = syCurrentTime.format("D MMMM YYYY");
+  sydneyTime.innerHTML = syCurrentTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
